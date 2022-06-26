LineageOS/android_device_xiaomi_sdm845-common

sdm845-common: Implement UDFPS handler
Change-Id: I7afff305140639d38ebb8bc51fbe790a1636db9e

udfps/UdfpsHandler.cpp
@@ -1,0 +1,95 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define LOG_TAG "UdfpsHander.xiaomi_sdm845"
+
+#include "UdfpsHandler.h"
+
+#include <android-base/logging.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <thread>
+#include <unistd.h>
+
+#define COMMAND_NIT 10
+#define PARAM_NIT_FOD 1
+#define PARAM_NIT_NONE 0
+
+#define FOD_UI_PATH "/sys/devices/platform/soc/soc:qcom,dsi-display@20/fod_ui"
+
+static bool readBool(int fd) {
+    char c;
+    int rc;
+
+    rc = lseek(fd, 0, SEEK_SET);
+    if (rc) {
+        LOG(ERROR) << "failed to seek fd, err: " << rc;
+        return false;
+    }
+
+    rc = read(fd, &c, sizeof(char));
+    if (rc != 1) {
+        LOG(ERROR) << "failed to read bool from fd, err: " << rc;
+        return false;
+    }
+
+    return c != '0';
+}
+
+class XiaomiUdfpsHander : public UdfpsHandler {
+  public:
+    void init(fingerprint_device_t *device) {
+        mDevice = device;
+
+        std::thread([this]() {
+            int fd = open(FOD_UI_PATH, O_RDONLY);
+            if (fd < 0) {
+                LOG(ERROR) << "failed to open fd, err: " << fd;
+                return;
+            }
+
+            struct pollfd fodUiPoll = {
+                    .fd = fd,
+                    .events = POLLERR | POLLPRI,
+                    .revents = 0,
+            };
+
+            while (true) {
+                int rc = poll(&fodUiPoll, 1, -1);
+                if (rc < 0) {
+                    LOG(ERROR) << "failed to poll fd, err: " << rc;
+                    continue;
+                }
+
+                mDevice->extCmd(mDevice, COMMAND_NIT,
+                                readBool(fd) ? PARAM_NIT_FOD : PARAM_NIT_NONE);
+            }
+        }).detach();
+    }
+
+    void onFingerDown(uint32_t /*x*/, uint32_t /*y*/, float /*minor*/, float /*major*/) {
+        // nothing
+    }
+
+    void onFingerUp() {
+        // nothing
+    }
+  private:
+    fingerprint_device_t *mDevice;
+};
+
+static UdfpsHandler* create() {
+    return new XiaomiUdfpsHander();
+}
+
+static void destroy(UdfpsHandler* handler) {
+    delete handler;
+}
+
+extern "C" UdfpsHandlerFactory UDFPS_HANDLER_FACTORY = {
+    .create = create,
+    .destroy = destroy,
+};

crdroidandroid/android_device_xiaomi_dipper-common

udfps/UdfpsHandler.cpp
@@ -1,0 +1,95 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define LOG_TAG "UdfpsHandler.xiaomi_sdm845"
+
+#include "UdfpsHandler.h"
+
+#include <android-base/logging.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <thread>
+#include <unistd.h>
+
+#define COMMAND_NIT 10
+#define PARAM_NIT_FOD 1
+#define PARAM_NIT_NONE 0
+
+#define FOD_UI_PATH "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/fod_ui"
+
+static bool readBool(int fd) {
+    char c;
+    int rc;
+
+    rc = lseek(fd, 0, SEEK_SET);
+    if (rc) {
+        LOG(ERROR) << "failed to seek fd, err: " << rc;
+        return false;
+    }
+
+    rc = read(fd, &c, sizeof(char));
+    if (rc != 1) {
+        LOG(ERROR) << "failed to read bool from fd, err: " << rc;
+        return false;
+    }
+
+    return c != '0';
+}
+
+class XiaomiUdfpsHander : public UdfpsHandler {
+  public:
+    void init(fingerprint_device_t *device) {
+        mDevice = device;
+
+        std::thread([this]() {
+            int fd = open(FOD_UI_PATH, O_RDONLY);
+            if (fd < 0) {
+                LOG(ERROR) << "failed to open fd, err: " << fd;
+                return;
+            }
+
+            struct pollfd fodUiPoll = {
+                    .fd = fd,
+                    .events = POLLERR | POLLPRI,
+                    .revents = 0,
+            };
+
+            while (true) {
+                int rc = poll(&fodUiPoll, 1, -1);
+                if (rc < 0) {
+                    LOG(ERROR) << "failed to poll fd, err: " << rc;
+                    continue;
+                }
+
+                mDevice->extCmd(mDevice, COMMAND_NIT,
+                                readBool(fd) ? PARAM_NIT_FOD : PARAM_NIT_NONE);
+            }
+        }).detach();
+    }
+
+    void onFingerDown(uint32_t /*x*/, uint32_t /*y*/, float /*minor*/, float /*major*/) {
+        // nothing
+    }
+
+    void onFingerUp() {
+        // nothing
+    }
+  private:
+    fingerprint_device_t *mDevice;
+};
+
+static UdfpsHandler* create() {
+    return new XiaomiUdfpsHander();
+}
+
+static void destroy(UdfpsHandler* handler) {
+    delete handler;
+}
+
+extern "C" UdfpsHandlerFactory UDFPS_HANDLER_FACTORY = {
+    .create = create,
+    .destroy = destroy,
+};